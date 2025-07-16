0ca38d2fcd35774d090fa5b4f0b4f691e51862a3
anhtri04 <134407440+anhtri04@users.noreply.github.com>
2025-07-16 14:35:44 +0700

    Update vite.config.ts
    update the repo path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Set base path for GitHub Pages deployment
-  base: mode === 'production' ? '/anhtri04.github.io/' : '/',
+  base: mode === 'production' ? 'anhtri04.github.io' : '/',
   server: {
     host: "::",
     port: 8080,
